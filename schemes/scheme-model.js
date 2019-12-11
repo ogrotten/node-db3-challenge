f9ac235ded2830675e9db176f73b869a9bab7b69
Darrin Lowery <lambda@monsto.com>
2019-12-11 17:07:30 -0600

    scaffolded scheme-model

---
+++ schemes/scheme-model.js
@@ -0,0 +1,34 @@
+const db = require("../data/db-config.js");
+
+module.exports = {
+	find,
+	findById,
+	findSteps,
+	add,
+	update,
+	remove
+}
+
+function find (id) {
+
+}
+
+function findById (id) {
+
+}
+
+function findSteps (id) {
+
+}
+
+function add (id) {
+
+}
+
+function update (id) {
+
+}
+
+function remove (id) {
+
+}
