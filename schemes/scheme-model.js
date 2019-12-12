48e276143d25fd9d86e98896ef7205f0433cd61f
Darrin Lowery <lambda@monsto.com>
2019-12-11 18:12:12 -0600

    completed route helpers

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -4,31 +4,59 @@ module.exports = {
 	find,
 	findById,
 	findSteps,
+	addStep,
 	add,
 	update,
 	remove
 }
 
-function find (id) {
-
+function find(id) {
+	return db("schemes")
 }
 
-function findById (id) {
-
+function findById(id) {
+	return db("schemes")
+		.where({ id })
+		.first();
 }
 
-function findSteps (id) {
-
+function findSteps(id) {
+	return db("steps")
+		.orderBy("steps.step_number")
+		.join("schemes", "steps.scheme_id", "schemes.id")
+		.where("schemes.id", id)
 }
 
-function add (id) {
-
+function addStep(stepData, id) {
+	return db("steps")
+		.insert(stepData, "id")
 }
 
-function update (id) {
-
+function add(data) {
+	return db("schemes")
+		.insert(data, "id")
+		.then(newid => {
+			const [id] = newid;
+			return findById(id);
+		});
 }
 
-function remove (id) {
+function update(changes, id) {
+	return db("schemes")
+		.where({ id })
+		.update(changes)
+		.then(ct => {
+			return findById(id);
+		});
+}
 
+function remove(id) {
+	const del = findById(id)
+		.then(delled => delled)
+	return db("schemes")
+		.where({ id })
+		.delete()
+		.then(ct => {
+			return del;
+		});
 }
